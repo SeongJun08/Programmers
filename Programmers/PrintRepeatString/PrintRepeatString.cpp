b3ddb55008a22d40a8cdf81dc20c9c47049e60dc
SeongJun08 <ellajune1612@gmail.com>
2024-06-16 20:29:35 +0900

    문자열 반복해서 출력하기

---
+++ Programmers/PrintRepeatString/PrintRepeatString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    std::string str;
+    int n;
+    
+    std::cin >> str >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << str;
+    }
+}
