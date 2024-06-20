2f486b832cb51bcb4b5fb043c6af63c44776fc50
SeongJun08 <ellajune1612@gmail.com>
2024-06-20 21:45:15 +0900

    문자열 섞어서 출력하기

---
+++ PrintMixString/PrintMixString.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+
+std::string solution(std::string str1, std::string str2) {
+    std::string answer = "";
+
+    int length = str1.length();
+
+    for (int i = 0; i < length; i++)
+    {
+        answer += str1[i];
+        answer += str2[i];
+    }
+
+    return answer;
+}
